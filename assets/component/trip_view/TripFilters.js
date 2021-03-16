f1b25ebe7a4c338c21318e418bf73460858f74eb
Marjo <marjorie.gestin2020@campus-eni.fr>
2021-03-16 02:01:35 +0100

    Build : Trip-View

--- assets/component/trip_view/TripFilters.js
+++ assets/component/trip_view/TripFilters.js
@@ -3,59 +3,91 @@ import TripList from "./TripList";
 
 class TripFilters extends Component {
 
-    render() {
+    state = {
+        campus : '',
+        searchNameTrip : '',
+        startDate : '',
+        endDate : '',
+    }
 
-        console.log()
+    handleCampus = e => {
+        this.setState({
+            campus : e.target.value,
+        })
+    }
+
+    handleSearchName = e => {
+        this.setState({
+            searchNameTrip : e.target.value,
+        })
+    }
+
+    handleStartDate = e => {
+        this.setState({
+            startDate : e.target.value,
+        })
+    }
+
+    handleEndDate = e => {
+        this.setState({
+            endDate : e.target.value,
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+    }
+
+    render() {
 
         return (
 
             <Fragment>
+                <h3>Filtrer les sorties</h3>
 
-            <h3>Filtrer les sorties</h3>
+                <form onSubmit={this.handleSubmit} id="trip-filters">
 
-            <form method="post" action="" id="trip-filters">
+                    <div className="col">
 
-                <div className="col">
+                        <div className="row">
+                            <label>Campus : </label>
+                            <select value={this.state.campus} onChange={this.handleCampus}>
+                                {/*TODO Afficher les campus via une rêquete ?*/ }
+                                <option>ST HERBLAIN</option>
+                                <option>LA ROCHE SUR YON</option>
+                            </select>
+                        </div>
+                        <div className="row">
+                            <label>Le nom de la sortie contient : </label>
+                            <input type="text" value={this.state.searchNameTrip} onChange={this.handleSearchName}/>
+                        </div>
+                        <div className="row">
+                            Entre <input type="date" value={this.state.startDate} onChange={this.handleStartDate}/> et <input type="date" value={this.state.endDate} onChange={this.handleEndDate}/>
+                        </div>
+                    </div>
 
+                    <div className="col">
+                        <div>
+                            <input type="checkbox" name="trip" value="organisation"/>
+                            <label htmlFor="coding">Sorties dont je suis l'organisateur.trice</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="trip" value="registered"/>
+                            <label htmlFor="music">Sorties auxquelles je suis inscrit.e</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="trip" value="not-registered"/>
+                            <label htmlFor="music">Sorties auxquelles je ne suis pas inscrit.e</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="trip" value="past-trip"/>
+                            <label htmlFor="music">Sorties passées</label>
+                        </div>
+                    </div>
+                    <button type="submit">Rechercher</button>
+                </form>
+                <TripList campus={this.state.campus} name={this.state.searchNameTrip} startDate={this.state.startDate} endDate={this.state.endDate}></TripList>
 
-                    <div className="row">
-                        <label>Campus : </label>
-                        <select>
-                            {/*TODO Afficher les campus via une rêquete ?*/ }
-                            <option>ST HERBLAIN</option>
-                            <option>LA ROCHE SUR YON</option>
-                        </select>
-                    </div>
-                    <div className="row">
-                        <label>Le nom de la sortie contient : </label>
-                        <input type="text"/>
-                    </div>
-                    <div className="row">
-                        Entre <input type="date"/> et <input type="date"/>
-                    </div>
-                </div>
-
-                <div className="col">
-                    <div>
-                        <input type="checkbox" name="trip" value="organisation"/>
-                        <label htmlFor="coding">Sorties dont je suis l'organisateur.trice</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" name="trip" value="registered"/>
-                        <label htmlFor="music">Sorties auxquelles je suis inscrit.e</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" name="trip" value="not-registered"/>
-                        <label htmlFor="music">Sorties auxquelles je ne suis pas inscrit.e</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" name="trip" value="past-trip"/>
-                        <label htmlFor="music">Sorties passées</label>
-                    </div>
-                </div>
-                <button type="submit">Rechercher</button>
-            </form>
-                <TripList search="{search}"></TripList>
             </Fragment>
 
         );
--- assets/component/trip_view/TripList.js
+++ assets/component/trip_view/TripList.js
@@ -2,20 +2,23 @@ import React, {Component} from 'react';
 
 
 class TripList extends Component {
-    render() {
-        return (
-            <div id="trip-list">
-                TESSST<br/>
-                TESSST<br/>
-                TESSST<br/>
-                TESSST<br/>
-                TESSST<br/>
-                TESSST<br/>
-            </div>
-        );
+
+        //TODO Axios recherche dans l'API
+
+
+        render() {
+                return (
+                    <div id="trip-list">
+                            TESSST<br/>
+                            TESSST<br/>
+                            TESSST<br/>
+                            TESSST<br/>
+                            TESSST<br/>
+                            TESSST<br/>
+                            {console.log(this.props)}
+                    </div>
+                );
     }
 }
-
-
 
 export default TripList;
