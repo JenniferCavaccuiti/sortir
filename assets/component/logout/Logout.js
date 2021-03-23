fc8f065773179a46eaa84f8b670821aaf2d3c650
GenieFaire <jennifer.cavaccuiti@yahoo.fr>
2021-03-23 07:23:56 +0100

    fix: redirect after logout ok

--- assets/component/logout/Logout.js
+++ assets/component/logout/Logout.js
@@ -1,15 +1,13 @@
 import React from "react";
 import axios from "axios";
-import  {Redirect} from "react-router-dom";
 
 const Logout = function (props) {
-        console.log('je rentre dans le logout');
-        localStorage.clear();
-        console.log('localstorage vide');
-        axios.get('/logout').then(r => {
-                    // this.props.history.push('/app');
-        });
-        // return <Redirect to="/app"/>;
+    console.log('je rentre dans le logout');
+    localStorage.clear();
+    console.log('localstorage vide');
+    axios.get('/logout').then(r => {
+        window.location.href = '/app';
+    });
 }
 
 export default Logout;
