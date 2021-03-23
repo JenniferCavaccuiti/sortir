972c79589fe2049d23d7466f60c85620424302fd
Marjo <marjorie.gestin2020@campus-eni.fr>
2021-03-23 02:03:57 +0100

    fix : Menu/Redirection

--- assets/component/Container.js
+++ assets/component/Container.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import LoginApp from "./connexion/LoginApp";
 import LoginForm from "./connexion/LoginForm";
